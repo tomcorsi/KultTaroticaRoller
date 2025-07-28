e7dbb8dd5ecdb7a44774423566848480d08f864f
tomcorsi <106984720+tomcorsi@users.noreply.github.com>
2025-07-28 23:03:56 +0100

    feat: added dependencies

--- src/taroticum.js
+++ src/taroticum.js
@@ -1,50 +1,50 @@
-const suits = ['Skulls', 'Roses', 'Hourglasses', 'Crescents', 'Eyes']
+const suits = ["Skulls", "Roses", "Hourglasses", "Crescents", "Eyes"];
 
 const majorArcana = [
-    'Anthropos',
-    'Demiurge',
-    'Astaroth',
-    'Kether',
-    'Chokmah',
-    'Binah',
-    'Chesed',
-    'Geburah',
-    'Tiphareth',
-    'Netzach',
-    'Hod',
-    'Yesod',
-    'Malkuth',
-    'Thaumiel',
-    'Chagidiel',
-    'Sathariel',
-    'Gamichicoth',
-    'Golab',
-    'Togarini',
-    'Hareb-Serap',
-    'Samael',
-    'Gamaliel',
-    'Nahemoth'
-]
+  "Anthropos",
+  "Demiurge",
+  "Astaroth",
+  "Kether",
+  "Chokmah",
+  "Binah",
+  "Chesed",
+  "Geburah",
+  "Tiphareth",
+  "Netzach",
+  "Hod",
+  "Yesod",
+  "Malkuth",
+  "Thaumiel",
+  "Chagidiel",
+  "Sathariel",
+  "Gamichicoth",
+  "Golab",
+  "Togarini",
+  "Hareb-Serap",
+  "Samael",
+  "Gamaliel",
+  "Nahemoth",
+];
 
 const drawMinor = () => {
-    const randomNumber = Math.floor(Math.random() * 9) + 1
-    const suit = Math.floor(Math.random() * 5)
-    return `${randomNumber} of ${suits[suit]}`
-} 
+  const randomNumber = Math.floor(Math.random() * 9) + 1;
+  const suit = Math.floor(Math.random() * 5);
+  return `${randomNumber} of ${suits[suit]}`;
+};
 
 const drawMajor = () => {
-    const randomNumber = Math.floor(Math.random() * 23)
-    return majorArcana[randomNumber]
-}
+  const randomNumber = Math.floor(Math.random() * 23);
+  return majorArcana[randomNumber];
+};
 
 const drawCard = () => {
-    const randomNumber = Math.floor(Math.random() * 45)
+  const randomNumber = Math.floor(Math.random() * 45);
 
-    if (randomNumber > 22) {
-        return drawMajor()
-    } else {
-        return drawMinor()
-    }
-}
+  if (randomNumber > 22) {
+    return drawMajor();
+  } else {
+    return drawMinor();
+  }
+};
 
-console.log(drawCard())
+console.log(drawCard());
